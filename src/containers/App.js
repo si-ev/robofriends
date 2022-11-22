9cdac1fe7fb9cd758c799f6cc2c4aa09b3c0b183
Simeon <yevlakhov@gmail.com>
2022-11-22 06:12:34 +0400

    Fix typos

--- src/containers/App.js
+++ src/containers/App.js
@@ -12,7 +12,7 @@ const mapStateToProps = state => {
      return{
           searchField: state.searchRobots.searchField,
           robots: state.requestRobots.robots,
-          isPending: state:requestRobots.isPending,
+          isPending: state.requestRobots.isPending,
           error: state.requestRobots.error
      }
 }
